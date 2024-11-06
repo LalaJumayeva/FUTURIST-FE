a9234319b607988622237e5695005da4d607fc07
Lala Jumayeva <cumayevalala@gmail.com>
2024-11-06 02:49:25 +0100

    added map to contact screen

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -63,7 +63,7 @@ function Contact() {
                 </div>
                 <div className="w-1/2 p-4 flex items-center flex-col justify-between py-16 pl-8">
                     <div className="flex flex-col">
-                        <p className="text-6xl font-Montserrat text-slate-50 mb-6">Contact Us</p>
+                        <p className="text-6xl font-Montserrat font-semibold text-slate-50 mb-6">Contact Us</p>
                         <p className="text-slate-300 text-lg font-Montserrat mb-4">We’d love to hear from you! Whether you have questions, feedback, or need assistance,
                             our team is here to help. Please fill out the form, and we’ll get back to you as soon as possible.</p>
                         <p className="text-slate-300 text-lg font-Montserrat mb-4">+1 123 456 7890</p>
@@ -84,6 +84,46 @@ function Contact() {
                             <p className="text-lg font-bold font-Montserrat text-slate-50 mb-6">Media Inquiries</p>
                             <p className="text-slate-300 text-sm font-Montserrat mb-4">If you’re a member of the media looking for information, interviews, or press materials, please reach out using the form.</p>
                         </div>
+                    </div>
+                </div>
+            </section>
+            <section className="flex flex-row items-center pl-8 pt-10 pb-24 relative">
+                <div className="absolute bottom-36 right-16 w-64 h-64 z-0 bg-blue-300 rounded-full filter blur-2xl opacity-50 animate-spinCounterClockwise"></div>
+                <div className="absolute bottom-64 right-8 w-80 h-72 z-0 bg-purple-300 rounded-full filter blur-2xl opacity-60 animate-spinClockwise"></div>
+
+                <div className="google-map-code">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3021.870558405966!2d-73.9851305845948!3d40.75889537932643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25855d5afbbb9%3A0x1f1e6a64cf04cd63!2sTimes%20Square!5e0!3m2!1sen!2sus!4v1587818542745!5m2!1sen!2sus"
+                        width="600"
+                        height="450"
+                        style={{ border: 0, borderRadius: 10 }}
+                        allowFullScreen=""
+                        aria-hidden="false"
+                        tabIndex="0"
+                        title="map"
+                    />
+                </div>
+                <div className="text-slate-50 ml-10 relative z-20">
+                    <p className="text-xl mb-6 font-Montserrat">Our Location</p>
+                    <p className="text-5xl mb-6 font-semibold font-Montserrat">Connecting Near and Far</p>
+
+                    <div>
+                        <p className="text-lg mb-4 font-semibold font-Montserrat">Headquarters</p>
+                        <p className="text-slate-300 mb-1 font-Montserrat">
+                            Futurist Inc.
+                        </p>
+                        <p className="text-slate-300 mb-1 font-Montserrat">
+                            New York city, USA.
+                        </p>
+                        <p className="text-slate-300 mb-1 font-Montserrat">
+                            7693 Thomas Ave.
+                        </p>
+                        <p className="text-slate-300 mb-1 font-Montserrat">
+                            Brooklyn, NY 11214
+                        </p>
+                        <p className="text-slate-300 mb-1 font-Montserrat">
+                            United States
+                        </p>
                     </div>
                 </div>
             </section>
